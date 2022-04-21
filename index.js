db06ba77925cc498506e47afb91a7e3820eca579
Zuzike Nemesová <darigjorkinen@gmail.com>
2022-04-21 21:27:45 +0200

    Tahy hráčov v js

---
+++ index.js
@@ -0,0 +1,26 @@
+//v js: nereagovať, ak je v políčku už symbol
+//pri klik sa mení styl políčka, ale po zídení sa vráti do pôvod. podoby
+
+/*const playerNav = document.querySelector('.hrac__nav');
+playerNav.innerHTML = `<p>HRAJE:</p>
+<img class="ikona__hra" src="${player}.svg" alt="${player}-icon"/>`*/
+
+const btnElm = document.querySelectorAll('button');
+
+let player = 'circle';
+
+for (let i = 0; i < btnElm.length; i += 1) {
+  btnElm[i].addEventListener('click', (ev) => {
+    if (player === 'circle') {
+      ev.target.classList.add('board__field--circle');
+      const playerIcon = document.querySelector('.ikona__hra');
+      playerIcon.src = 'cross.svg';
+      player = 'cross';
+    } else {
+      ev.target.classList.add('board__field--cross');
+      const playerIcon = document.querySelector('.ikona__hra');
+      playerIcon.src = 'circle.svg';
+      player = 'circle';
+    }
+  });
+}

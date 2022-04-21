5a0e3d39061a2227b4e3ebe72d8ad5d4b2b759be
Zuzike Nemesová <darigjorkinen@gmail.com>
2022-04-21 21:43:12 +0200

    Políčka disabled

--- index.js
+++ index.js
@@ -5,8 +5,24 @@
 playerNav.innerHTML = `<p>HRAJE:</p>
 <img class="ikona__hra" src="${player}.svg" alt="${player}-icon"/>`*/
 
-const btnElm = document.querySelectorAll('button');
 
+/* btnElm.forEach((btn)=>
+  btn.addEventListener('click',(ev)=>{
+    if (player === 'circle') {
+      ev.target.classList.add('board__field--circle');
+      const playerIcon = document.querySelector('.ikona__hra');
+      playerIcon.src = 'cross.svg';
+      player = 'cross';
+    }
+    else {
+        ev.target.classList.add('board__field--cross');
+        const playerIcon = document.querySelector('.ikona__hra');
+        playerIcon.src = 'circle.svg';
+        player = 'circle';
+      }}),
+  ) */
+
+const btnElm = document.querySelectorAll('button');
 let player = 'circle';
 
 for (let i = 0; i < btnElm.length; i += 1) {
@@ -22,5 +38,6 @@ for (let i = 0; i < btnElm.length; i += 1) {
       playerIcon.src = 'circle.svg';
       player = 'circle';
     }
-  });
-}
+    btnElm[i].disabled = true
+    });
+   }
